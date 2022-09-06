c737a944b55544f558259799359b42aaa1cd894e
Usuario <Usuario@CPD03-M10>
2022-09-06 15:43:27 -0300

    Adicionar arquivos de projeto.

---
+++ Importar_Marcar/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Importar_Marcar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Importar = new System.Windows.Forms.Button();
+            this.btn_Marcar = new System.Windows.Forms.Button();
+            this.btn_Desmarcar = new System.Windows.Forms.Button();
+            this.dvg_despesa = new System.Windows.Forms.DataGridView();
+            this.ofdArquivo = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dvg_despesa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Importar
+            // 
+            this.btn_Importar.Location = new System.Drawing.Point(22, 12);
+            this.btn_Importar.Name = "btn_Importar";
+            this.btn_Importar.Size = new System.Drawing.Size(201, 53);
+            this.btn_Importar.TabIndex = 0;
+            this.btn_Importar.Text = "Importar Arquivo";
+            this.btn_Importar.UseVisualStyleBackColor = true;
+            this.btn_Importar.Click += new System.EventHandler(this.btn_Importar_Click);
+            // 
+            // btn_Marcar
+            // 
+            this.btn_Marcar.Location = new System.Drawing.Point(917, 29);
+            this.btn_Marcar.Name = "btn_Marcar";
+            this.btn_Marcar.Size = new System.Drawing.Size(120, 36);
+            this.btn_Marcar.TabIndex = 1;
+            this.btn_Marcar.Text = "Marcar Todos";
+            this.btn_Marcar.UseVisualStyleBackColor = true;
+            this.btn_Marcar.Click += new System.EventHandler(this.btn_Marcar_Click);
+            // 
+            // btn_Desmarcar
+            // 
+            this.btn_Desmarcar.Location = new System.Drawing.Point(1078, 29);
+            this.btn_Desmarcar.Name = "btn_Desmarcar";
+            this.btn_Desmarcar.Size = new System.Drawing.Size(120, 36);
+            this.btn_Desmarcar.TabIndex = 2;
+            this.btn_Desmarcar.Text = "Desmarcar Todos";
+            this.btn_Desmarcar.UseVisualStyleBackColor = true;
+            this.btn_Desmarcar.Click += new System.EventHandler(this.btn_Desmarcar_Click);
+            // 
+            // dvg_despesa
+            // 
+            this.dvg_despesa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvg_despesa.Location = new System.Drawing.Point(25, 83);
+            this.dvg_despesa.Name = "dvg_despesa";
+            this.dvg_despesa.Size = new System.Drawing.Size(1173, 480);
+            this.dvg_despesa.TabIndex = 3;
+            this.dvg_despesa.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dvg_despesa_CellClick);
+            // 
+            // ofdArquivo
+            // 
+            this.ofdArquivo.FileName = "openFileDialog1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 638);
+            this.Controls.Add(this.dvg_despesa);
+            this.Controls.Add(this.btn_Desmarcar);
+            this.Controls.Add(this.btn_Marcar);
+            this.Controls.Add(this.btn_Importar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dvg_despesa)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Importar;
+        private System.Windows.Forms.Button btn_Marcar;
+        private System.Windows.Forms.Button btn_Desmarcar;
+        private System.Windows.Forms.DataGridView dvg_despesa;
+        private System.Windows.Forms.OpenFileDialog ofdArquivo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Importar_Marcar/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Importar_Marcar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Cria as coluunas definindo o tipo de cada uma
+            dvg_despesa.Columns.Insert(0, new DataGridViewCheckBoxColumn());
+            dvg_despesa.Columns.Insert(1, new DataGridViewTextBoxColumn());
+            dvg_despesa.Columns.Insert(2, new DataGridViewTextBoxColumn());
+            dvg_despesa.Columns.Insert(3, new DataGridViewTextBoxColumn());
+            dvg_despesa.Columns.Insert(4, new DataGridViewTextBoxColumn());
+            dvg_despesa.Columns.Insert(5, new DataGridViewTextBoxColumn());
+
+            // Renomeia as colunas
+            dvg_despesa.Columns[0].Name = "OK";
+            dvg_despesa.Columns[1].Name = "CODIGO";
+            dvg_despesa.Columns[2].Name = "PRODUTO";
+            dvg_despesa.Columns[3].Name = "VALOR_COMPRA";
+            dvg_despesa.Columns[4].Name = "VALOR_VENDA";
+            dvg_despesa.Columns[5].Name = "ESTOQUE";
+
+            // Configura a DataGridView
+            dvg_despesa.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dvg_despesa.ReadOnly = true;
+            dvg_despesa.AllowUserToAddRows = false;
+            dvg_despesa.AllowUserToDeleteRows = false;
+            dvg_despesa.AllowUserToOrderColumns = true;
+
+            // Configura a coluna de valor formatando e alinhado
+            dvg_despesa.Columns[3].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dvg_despesa.Columns[3].DefaultCellStyle.Format = "###,###,##0.00";
+
+            dvg_despesa.Columns[4].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dvg_despesa.Columns[4].DefaultCellStyle.Format = "###,###,##0.00";
+
+        }
+
+
+
+        private void btn_Importar_Click(object sender, EventArgs e)
+        {
+            dvg_despesa.RowCount = 0;
+
+            ofdArquivo.FileName = "";
+            ofdArquivo.ShowDialog();
+
+            StreamReader arquivo = File.OpenText(ofdArquivo.FileName);
+
+            String linha;
+            while ((linha = arquivo.ReadLine()) != null)
+            {
+                string[] dados = linha.Split(';');
+                int codigo = int.Parse(dados[0]);
+                string produto = dados[1];
+                double valor_compra = double.Parse(dados[2]);
+                double valor_venda = double.Parse(dados[3]);
+                int estoque = int.Parse(dados[4]);
+
+                dvg_despesa.Rows.Add(false, codigo, produto, valor_compra, valor_venda, estoque);
+            }
+        }
+
+        private void dvg_despesa_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dvg_despesa.RowCount > 0 && e.ColumnIndex == 0)
+            {
+                if (Convert.ToBoolean(dvg_despesa.CurrentRow.Cells[0].Value) == false)
+                {
+                    dvg_despesa.CurrentRow.Cells[0].Value = true;
+                }
+                else
+                {
+                    dvg_despesa.CurrentRow.Cells[0].Value = false;
+                }
+            }
+        }
+
+
+        private void btn_Marcar_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow linha in dvg_despesa.Rows)
+                linha.Cells[0].Value = true;
+        }
+
+        private void btn_Desmarcar_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow linha in dvg_despesa.Rows)
+                linha.Cells[0].Value = false;
+        }
+    }
+}
